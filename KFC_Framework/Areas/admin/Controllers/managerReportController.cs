120ef9384ba0c1b7199f3a1f64b01c9803163348
KhaZox <Khapro0066@gmail.com>
2024-02-28 12:32:13 +0700

    View User

--- KFC_Framework/Areas/admin/Controllers/managerReportController.cs
+++ KFC_Framework/Areas/admin/Controllers/managerReportController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,14 +7,14 @@ using System.Web.Mvc;
 
 namespace KFC_Framework.Areas.admin.Controllers
 {
-    public class managerProductController : Controller
+    public class managerReportController : Controller
     {
-        // GET: admin/managerProduct/inforProduct
-        public ActionResult inforProduct()
+        // GET: admin/managerReport
+        public ActionResult salesReport()
         {
             return View();
         }
-        public ActionResult addProduct()
+        public ActionResult ordersReport()
         {
             return View();
         }
---
+++ KFC_Framework/Areas/admin/Controllers/managerWarehouseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KFC_Framework.Areas.admin.Controllers
+{
+    public class managerWarehouseController : Controller
+    {
+        // GET: admin/managerWarehouse
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
